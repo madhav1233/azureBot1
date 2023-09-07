d194f5534732e22243a94e30026fbe325c803a84
madhav1233 <52925906+madhav1233@users.noreply.github.com>
2023-09-08 07:09:38 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, jsonify
 from botbuilder.core import BotFrameworkAdapter, BotFrameworkAdapterSettings, TurnContext
-from botbuilder.schema import Activity
 from bot.mybot import MyBot
 import os
 import logging
-import asyncio
-
 
 app = Flask(__name__)
 
@@ -21,30 +18,17 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route("/api/messages", methods=["POST"])
 def messages():
-    print("Headers:", request.headers)
-    print("Body:", request.data)
     if "application/json" in request.headers["Content-Type"]:
         body = request.json
-        print(body)
-        
-        activity = Activity.deserialize(body)
-        activity.service_url = body.get("service_url")
-        conversation_reference = TurnContext.get_conversation_reference(activity)
-        activity = TurnContext.apply_conversation_reference(activity, conversation_reference)
-        
-        async def call_bot_framework():
-            try:
-                await ADAPTER.process_activity(activity, "", BOT.on_turn)
-            except Exception as exception:
-                print("Error:", str(exception))
-                raise
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(call_bot_framework())
-        return jsonify({"status": "Message processed"}), 200
     else:
         return jsonify({"error": "Expecting application/json in request header's Content-Type."}), 400
 
+    async def call_bot_framework():
+        activity = TurnContext.apply_conversation_reference(body, BotFrameworkAdapter.get_conversation_reference(body))
+        await ADAPTER.process_activity(activity, BOT.on_turn)
+
+    loop.run_until_complete(call_bot_framework())
+    return jsonify({"status": "Message processed"}), 200
 
 if __name__ == "__main__":
     app.run(port=3978)
